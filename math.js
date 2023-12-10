cebe36e0ddc496be0aca980d16ccd4a04cd08b51
Meganathan <meganathan212003@gmail.com>
2023-12-10 13:52:01 +0530

    added new files

---
+++ math.js
@@ -0,0 +1,14 @@
+/**
+ * In this below the code we 
+ * pass the two arugment it's number now we want
+ * to export the module and import the another file.
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ */
+function add(a, b) {
+    return a + b;
+}
+
+
+module.exports = {add}; // We can acheive using the module function
--- server.js
+++ server.js
@@ -10,20 +10,21 @@
 const { log } = require('console');
 const os = require('os'); // This is called as module
 const path = require('path');
+const math = require('./math');
 
-console.log(os.homedir()); // These are the type form my os
+console.log(os.homedir()); // These are the method return information from my OS.
 console.log(os.type());
 console.log(os.version());
 console.log(os.platform());
 console.log(os.hostname());
 console.log(os.userInfo());
 
-console.log(__dirname);
-console.log(__filename);
+log(__dirname);
 
 console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename)); // this will return the extention of your file
 
-
+// Below code we can use the math.js here
+log(math.add(2,5)); // output is 5
 
