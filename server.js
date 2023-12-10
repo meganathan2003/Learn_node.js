a59ef749a952e6c09a476e117687d984c34060df
Meganathan Subramanian <meganathan212003@gmail.com>
2023-12-10 13:17:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+
+// Below the code for the moudle
+
+const os = require('os');
+
+console.log(os.homedir()); // These are the type form my os
+console.log(os.type());
+console.log(os.version());
+console.log(os.platform());
+console.log(os.hostname());
+console.log(os.userInfo());
+
